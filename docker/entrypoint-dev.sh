79693f6a31fd1e976a9a907e1ef4494bd0d7e8da
Brandon Bakker <brandon@bdmd.com.au>
2025-12-07 12:45:25 +1000

    feat: enhance Docker setup with user-specific UID/GID for non-root execution

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -4,7 +4,11 @@
 
 set -e
 
-poetry install --no-interaction --no-ansi
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install the package for the current user without touching system or mounted files owned by root
+# pip install --user --no-deps --editable /app
+poetry install  --no-interaction --no-ansi
 
 echo "Ready! Working directory: $(pwd)"
 
